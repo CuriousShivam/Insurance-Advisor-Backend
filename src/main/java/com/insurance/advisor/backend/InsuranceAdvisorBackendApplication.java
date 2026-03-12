f9dc30a569fdf48bc105fbfa0cf5dd08bd031913
CuriousShivam <shivam.sah2023t@gmail.com>
2026-03-12 16:04:45 +0530

    Initial Commit

---
+++ src/main/java/com/insurance/advisor/backend/InsuranceAdvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.advisor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAdvisorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceAdvisorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insurance/advisor/backend/InsuranceAdvisorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.advisor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAdvisorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
